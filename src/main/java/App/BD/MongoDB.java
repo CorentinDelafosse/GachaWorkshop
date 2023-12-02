bfcf6ec46fbbb252e64b5fbd50d8deb885dee657
CorentinDelafosse <90545467+CorentinDelafosse@users.noreply.github.com>
2023-12-02 14:52:51 +0100

    Ajout recherche article Selon nom et desc + delete article

--- src/main/java/App/BD/MongoDB.java
+++ src/main/java/App/BD/MongoDB.java
@@ -6,7 +6,11 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.bson.Document;
+import org.bson.types.ObjectId;
 
 public class MongoDB {
     private static final String URL = "mongodb://localhost:27017";
@@ -36,4 +40,100 @@ public class MongoDB {
             System.err.println("Erreur lors de la connexion à la base de données : " + e.getMessage());
         }
     }
+
+    // fonction pour la recherche d'article selon le nom ou le prix
+    public static void searchProduct(String nomProduit, double prixProduit) {
+        // Connectez-vous à la base de données MongoDB
+        try (MongoClient mongoClient = MongoClients.create(URL)) {
+            // Sélectionnez la base de données
+            MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+
+            // Sélectionnez la collection
+            MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
+
+            // Recherchez les documents correspondants
+            Document query = new Document();
+            if (nomProduit != null && !nomProduit.isEmpty()) {
+                query.append("nom", nomProduit);
+            }
+            if (prixProduit != 0) {
+                query.append("prix", prixProduit);
+            }
+
+            // Affichez les documents correspondants
+            collection.find(query).forEach(document -> System.out.println(document.toJson()));
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la connexion à la base de données : " + e.getMessage());
+        }
+    }
+
+
+    public static List<Document> getAllArticles() {
+        List<Document> articles = new ArrayList<>();
+
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+            // Sélectionnez la base de données
+            MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+
+            // Sélectionnez la collection
+            MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
+
+            // Récupérez tous les documents dans la collection
+            for (Document document : collection.find()) {
+                articles.add(document);
+            }
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la récupération des articles : " + e.getMessage());
+        }
+
+        return articles;
+    }
+
+    public static void deleteArticle(String articleId) {
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+
+            System.out.println("Article ID : " + articleId);
+            // Sélectionnez la base de données
+            MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+
+            // Sélectionnez la collection
+            MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
+
+            // Supprimez l'article en utilisant son ID
+            collection.deleteOne(new Document("_id", new ObjectId(articleId)));
+            
+            System.out.println("Article supprimé avec succès.");
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la suppression de l'article : " + e.getMessage());
+        }
+    }
+
+
+    public static List<Document> rechercheArticle(String rechercheTerm) {
+        List<Document> Articles = new ArrayList<>();
+    
+        try (MongoClient mongoClient = MongoClients.create("mongodb://localhost:27017")) {
+            // Sélectionnez la base de données
+            MongoDatabase database = mongoClient.getDatabase(DATABASE_NAME);
+    
+            // Sélectionnez la collection
+            MongoCollection<Document> collection = database.getCollection(COLLECTION_NAME);
+    
+            // Effectuez la recherche d'articles en fonction du terme spécifié
+            Document query = new Document();
+            query.append("$or", List.of(
+                    new Document("nom", new Document("$regex", rechercheTerm).append("$options", "i")),
+                    new Document("description", new Document("$regex", rechercheTerm).append("$options", "i"))
+            ));
+    
+            // Récupérez les documents filtrés dans la collection
+            collection.find(query).forEach(Articles::add);
+    
+        } catch (Exception e) {
+            System.err.println("Erreur lors de la recherche des articles : " + e.getMessage());
+        }
+    
+        return Articles;
+    }
+    
 }
--- src/main/java/App/controllers/RechercheController.java
+++ src/main/java/App/controllers/RechercheController.java
@@ -1,22 +1,58 @@
 package App.controllers;
 
+import java.util.List;
+
+import org.bson.Document;
+import org.bson.types.ObjectId;
 // Importez les classes nécessaires
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import App.BD.MongoDB;
 
 // Controller affichant la page d'ajout d'article FrontEnd/NewArticle.html
 @Controller 
 public class RechercheController {
 
     @GetMapping("/recherche")
-    public String recherche(Model model) {
+    public String searchArticle(Model model) {
+        // Implémentez la logique de recherche et récupérez la liste d'articles
+        List<Document> articles = MongoDB.getAllArticles(); // Assurez-vous d'implémenter cette méthode dans votre classe MongoDB
+
+        // Ajoutez la liste d'articles au modèle
+        model.addAttribute("articles", articles);
+
         return "recherche";
     }
 
     @PostMapping("/recherche")
-    public String rechercheSubmit(@ModelAttribute RechercheController recherche, Model model) {
-        model.addAttribute("recherche", recherche);
+    public String deleteArticle(@RequestParam("articleId") String articleId) {
+        // Implémentez la logique de suppression de l'article
+
+        System.out.println("Article à supprimer : " + articleId);
+        // le meme print mais en ObjectId de mongoDB
+        System.out.println("Article à supprimer : " + new ObjectId(articleId));
+        MongoDB.deleteArticle(articleId); // Assurez-vous d'implémenter cette méthode dans votre classe MongoDB
+
+        // Redirigez vers la page de recherche d'articles après la suppression
+        return "redirect:/recherche";
+    }
+
+
+    @GetMapping("/rechercheArticle")
+    public String rechercheArticle(@RequestParam(name = "rechercheTerm", required = false) String rechercheTerm,
+                                Model model) {
+        // Si aucun terme de recherche n'est spécifié, récupérez tous les articles
+        if (rechercheTerm == null || rechercheTerm.isEmpty()) {
+            return "redirect:/recherche";
+        }
+
+        // Implémentez la logique de recherche et récupérez la liste d'articles filtrés
+        List<Document> filteredArticles = MongoDB.rechercheArticle(rechercheTerm); // Assurez-vous d'implémenter cette méthode dans votre classe MongoDB
+
+        // Ajoutez la liste d'articles filtrés au modèle
+        model.addAttribute("articles", filteredArticles);
+
         return "recherche";
     }
 
