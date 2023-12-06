bdec22b94700af8b10999e1005e4c9ab1816d93b
CorentinDelafosse <90545467+CorentinDelafosse@users.noreply.github.com>
2023-12-06 19:51:39 +0100

    fichier CSV (nom;desc;prix)

--- src/main/java/App/controllers/NewArticleController.java
+++ src/main/java/App/controllers/NewArticleController.java
@@ -1,9 +1,20 @@
 package App.controllers;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 // Importez les classes nécessaires
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
 import App.BD.MongoDB;
 
 // Controller affichant la page d'ajout d'article FrontEnd/NewArticle.html
@@ -30,6 +41,47 @@ public class NewArticleController {
         model.addAttribute("prix", prix);
 
         return "newArticle";
+    }
+
+    @PostMapping("/newArticleCSV")
+    public String newArticleCSV(@RequestParam("file") MultipartFile file, 
+                                      Model model) {
+
+        
+        // Assurez-vous que le fichier n'est pas vide
+        if (file.isEmpty()) {
+            System.out.println("iciError");
+            // Gérer le cas où le fichier est vide
+            return "redirect:/errorPage"; // Redirigez vers une page d'erreur appropriée
+        }
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
+
+            List<List<String>> records = new ArrayList<>();
+            String ligne;
+            while((ligne = br.readLine()) != null) {
+                String[] values = ligne.split(";");
+                records.add(Arrays.asList(values));
+            }
+
+            System.out.println("ici");
+            for (List<String> record : records) {
+                String nomProduit = record.get(0);
+                String descriptionProduit = record.get(1);
+                double prixProduit = Double.parseDouble(record.get(2));
+
+                System.out.println("nomProduit : " + nomProduit);
+                System.out.println("descriptionProduit : " + descriptionProduit);
+                System.out.println("prixProduit : " + prixProduit);
+
+                MongoDB.addProduct(nomProduit, descriptionProduit, prixProduit);
+            }
+
+            return "newArticle";
+        } catch (IOException e) {
+            // Gérer les exceptions liées à la lecture du fichier
+            e.printStackTrace(); // Vous pouvez également rediriger vers une page d'erreur appropriée
+            return "redirect:/errorPage";
+        }
     }
 
 }
